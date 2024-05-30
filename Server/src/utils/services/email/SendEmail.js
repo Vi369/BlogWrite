3a1a9daad981184fae99b6b099996f8f400b6525
Vikas singh varma <Vikashverma3001@gmail.com>
2024-05-30 18:08:58 +0530

    email send utils completed

--- Server/src/utils/services/email/SendEmail.js
+++ Server/src/utils/services/email/SendEmail.js
@@ -1,18 +1,20 @@
 import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-// TODO: TO send any kind of email modify 
-const SendEmail = async()=>{
+
+const SendEmail = async(email,subject,emailType)=>{
 try {
-    await resend.emails.send({
+    const response = await resend.emails.send({
         from: process.env.RESEND_ADMIN_EMAIL,
         to: email,
         subject: subject,
-        react: <EmailTemplate firstName="John" product="MyApp" />,
+        react: emailType,
     });
+    return response;
 
 } catch (emailError) {
-    
+    console.log("Error during sending email:", emailError);
+    return null;
 }
 }
 
